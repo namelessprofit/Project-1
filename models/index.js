66b391d49b62d0812261ce3af453bee8689c2075
namelessprofit <posadaarchy@gmail.com>
2017-03-16 13:40:42 -0700

    sending stuff

--- models/index.js
+++ models/index.js
@@ -6,4 +6,4 @@ module.exports.User = require('./user');
 
 module.exports.Entry = require('./entry');
 
-mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/healthy_test");
+// mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/healthy_test");
